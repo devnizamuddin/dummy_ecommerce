06407599ab9a457928e7ae7272fbc4f63baa4aa6
devnizamuddin <dev.nizamuddin@gmail.com>
2026-02-23 06:30:25 +0600

    🆕 Feature: ApiCommunication

---
+++ lib/core/services/api_communication.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+import '../constant/app_constant.dart';
+import '../models/api_response.dart';
+
+class ApiCommunication {
+  late final Dio _dio;
+
+  ApiCommunication({
+    Duration connectTimeout = const Duration(seconds: 60),
+    Duration receiveTimeout = const Duration(seconds: 60),
+  }) {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: kBaseUrl,
+        connectTimeout: connectTimeout,
+        receiveTimeout: receiveTimeout,
+        responseType: ResponseType.json,
+      ),
+    );
+
+    if (kDebugMode) {
+      _dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+          responseBody: true,
+        ),
+      );
+    }
+  }
+  //* ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  //* ┃                             GET Request                                ┃
+  //* ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+  Future<ApiResponse<T>> doGetRequest<T>({
+    required String endpoint,
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    final response = await _dio.get(
+      endpoint,
+      queryParameters: queryParameters,
+    );
+    return ApiResponse<T>(
+      statusCode: response.statusCode ?? kUnknownErrorCode,
+      data: response.data,
+      isSuccess: response.statusCode == 200,
+    );
+  }
+
+  //* ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+  //* ┃ Optional                                  ┃
+  //* ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+  void dispose() {
+    _dio.close(force: true);
+  }
+}
