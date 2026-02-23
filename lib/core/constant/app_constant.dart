fb45572fc62d42c91e8fdd7270d98656b04a10af
devnizamuddin <dev.nizamuddin@gmail.com>
2026-02-23 06:32:22 +0600

    🆕 Feature: Constants added

---
+++ lib/core/constant/app_constant.dart
@@ -0,0 +1,7 @@
+const String kAppName = 'Dummy Ecommerce';
+const String kBaseUrl = 'https://dummyjson.com';
+const int kDefaultLimit = 10;
+const int kDefaultSkip = 0;
+const int kDefaultPage = 1;
+const int kUnknownErrorCode = 0;
+const String kUnknownError = 'Something went wrong. Please try again later.';
